a3e8eeb99699357a1dadb04be65d8248b21997da
issaa <issa.aldzhamal@gmail.com>
2019-05-13 21:01:13 +0300

    Toolbar

--- source/Classify/lib/presentation/ui/screens/main/main_screen_view_model.dart
+++ source/Classify/lib/presentation/ui/screens/main/main_screen_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:classify/presentation/ui/screens/base/mvvm/stateful/app_view_model.dart';
 import 'package:classify/presentation/ui/screens/main/main_screen_model.dart';
 import 'package:classify/presentation/ui/screens/main/main_screen_view.dart';
+import 'package:flutter/animation.dart';
 
 class MainScreenViewModel
     extends AppViewModel<MainScreenModel, MainScreenView> {
@@ -9,8 +10,17 @@ class MainScreenViewModel
   @override
   init() async {
     super.init();
+    model.onNavigateToPage.addCallbackObject(changePageIndex);
   }
 
   @override
   void preferenceInit() async {}
+
+
+  void changePageIndex(int index) {
+    model.currentPageIndex = index;
+    model.pageController.animateToPage(index,
+        duration: const Duration(milliseconds: 1), curve: Curves.linear);
+    view.updateUI();
+  }
 }
