e6cdd3e0ba0320f8b98006369fcac6ea7de13751
vblago <vblago.ltd@gmail.com>
2019-05-14 17:21:56 +0300

    subjects screen ui

--- source/Classify/lib/presentation/utils/utils.dart
+++ source/Classify/lib/presentation/utils/utils.dart
@@ -3,69 +3,69 @@ import 'package:flutter/material.dart';
 class Utils {
   static List<ButtonStyle> subjectListButtons = [
     ButtonStyle(
-        "Computing",
+        "History",
         LinearGradient(
             colors: [Colors.greenAccent[400], Colors.greenAccent[400]]),
         "1"),
     ButtonStyle(
-        "Art",
+        "English",
         LinearGradient(
             colors: [Colors.deepOrange[600], Colors.deepOrange[600]]),
         "2"),
-    ButtonStyle("History",
+    ButtonStyle("Math",
         LinearGradient(colors: [Colors.yellow[700], Colors.yellow[700]]), "3"),
-    ButtonStyle("English",
-        LinearGradient(colors: [Colors.purple[600], Colors.purple[600]]), "4"),
-    ButtonStyle("Maths",
-        LinearGradient(colors: [Colors.red[600], Colors.red[600]]), "5"),
     ButtonStyle("PE",
-        LinearGradient(colors: [Colors.teal[600], Colors.teal[600]]), "6"),
+        LinearGradient(colors: [Colors.purple[600], Colors.purple[600]]), "4"),
     ButtonStyle("Biology",
-        LinearGradient(colors: [Colors.green[600], Colors.green[600]]), "7"),
+        LinearGradient(colors: [Colors.red[600], Colors.red[600]]), "5"),
     ButtonStyle("Chemistry",
-        LinearGradient(colors: [Colors.blue[600], Colors.blue[600]]), "8"),
+        LinearGradient(colors: [Colors.teal[600], Colors.teal[600]]), "6"),
+    ButtonStyle("Computer Science",
+        LinearGradient(colors: [Colors.green[600], Colors.green[600]]), "7"),
     ButtonStyle("Philosophy",
+        LinearGradient(colors: [Colors.blue[600], Colors.blue[600]]), "8"),
+    ButtonStyle("Economics",
         LinearGradient(colors: [Colors.pink[600], Colors.pink[600]]), "9"),
     ButtonStyle(
-        "Music",
+        "DT",
         LinearGradient(colors: [Colors.lightBlue[300], Colors.lightBlue[300]]),
         "10"),
-    ButtonStyle("DT",
+    ButtonStyle("Italian",
         LinearGradient(colors: [Colors.pink[300], Colors.pink[300]]), "11"),
-    ButtonStyle("Geography",
-        LinearGradient(colors: [Colors.lime[600], Colors.lime[600]]), "12"),
-    ButtonStyle("French",
-        LinearGradient(colors: [Colors.redAccent, Colors.redAccent]), "13"),
     ButtonStyle("Latin",
-        LinearGradient(colors: [Colors.orange[600], Colors.orange[600]]), "14"),
+        LinearGradient(colors: [Colors.lime[600], Colors.lime[600]]), "12"),
+    ButtonStyle("Geography",
+        LinearGradient(colors: [Colors.redAccent, Colors.redAccent]), "13"),
     ButtonStyle("Spanish",
+        LinearGradient(colors: [Colors.orange[600], Colors.orange[600]]), "14"),
+    ButtonStyle("German",
         LinearGradient(colors: [Colors.indigo[400], Colors.indigo[400]]), "15"),
     ButtonStyle(
-        "German", LinearGradient(colors: [Colors.black, Colors.black]), "16"),
-    ButtonStyle(
-        "Physics",
-        LinearGradient(
-          colors: [
-            Colors.grey[600],
-            Colors.grey[600],
-            Colors.grey[700],
-            Colors.grey[700],
-          ],
-          stops: [0.25, 0.50, 0.75, 1],
-        ),
-        "17"),
-    ButtonStyle(
-        "Drama",
-        LinearGradient(
-          colors: [
-            Colors.cyan[300],
-            Colors.cyan[400],
-            Colors.cyan[500],
-            Colors.cyan[600],
-          ],
-          stops: [0.25, 0.50, 0.75, 1],
-        ),
-        "18"),
+        "Physics", LinearGradient(colors: [Colors.black, Colors.black]), "16"),
+    // ButtonStyle(
+    //     "Physics",
+    //     LinearGradient(
+    //       colors: [
+    //         Colors.grey[600],
+    //         Colors.grey[600],
+    //         Colors.grey[700],
+    //         Colors.grey[700],
+    //       ],
+    //       stops: [0.25, 0.50, 0.75, 1],
+    //     ),
+    //     "17"),
+    // ButtonStyle(
+    //     "Drama",
+    //     LinearGradient(
+    //       colors: [
+    //         Colors.cyan[300],
+    //         Colors.cyan[400],
+    //         Colors.cyan[500],
+    //         Colors.cyan[600],
+    //       ],
+    //       stops: [0.25, 0.50, 0.75, 1],
+    //     ),
+    //     "18"),
   ];
 }
 
